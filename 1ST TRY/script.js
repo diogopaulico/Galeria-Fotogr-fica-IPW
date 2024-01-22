9018774a4763a34d6371545d10fe97059d92595d
Diogo Paulico <152753019+diogopaulico@users.noreply.github.com>
2024-01-22 17:25:56 +0000

    Add files via upload

--- 1ST TRY/script.js
+++ 1ST TRY/script.js
@@ -1,22 +1,3 @@
-function mensagem(){
-    let nome= document.getElementById("nome").value;
-    let mail= document.getElementById("mail").value;
-    let sugestao= document.getElementById("sugestao").value;
-    if(nome=="" || mail=="" || sugestao==""){
-        alert("Preencha os Campos");
-    }
-    else if(mail.indexOf('@') == -1){
-        alert("Email Inválido");
-    }
-    else{
-        alert("Obrigado "+nome+"! Em breve entraremos em contato com você pelo e-mail "+mail);
-    }
-}
-function limpar(){
-    document.getElementById("nome").value="";
-    document.getElementById("mail").value="";
-    document.getElementById("sugestao").value="";
-}
 // Codigo para o evento click ser usando e mostrar a imagem respetiva em fullscreen
 function mostrarLightbox(event) {
     let lightbox = document.getElementById('lightbox');
@@ -31,3 +12,37 @@ function esconderLightbox() {
 // Codigo para adicionar o evento de click em todas as imagens
 let imagens = document.querySelectorAll('img');
 imagens.forEach(img => img.addEventListener('click', mostrarLightbox));
+
+function mensagem(){
+    var from_name = document.getElementById("nome").value;
+    var email_id = document.getElementById("mail").value;
+    var message = document.getElementById("sugestao").value;
+    if(from_name == "" || email_id == "" || message == ""){
+        alert("Preencha os Campos");
+        return;
+    }
+    var params = {
+        'from_name': from_name,
+        'email_id': email_id,
+        'message': message,
+    };
+
+    if(email_id.indexOf('@') == -1){
+        alert("Email invalido");
+        return;
+    }
+    emailjs.send('service_xon81lf', 'template_tqdl9jl', params).then(function(res){
+        console.log('E-mail enviado com sucesso:', res);
+        alert("E-mail enviado com sucesso");
+        limpar();
+    }, function(error){
+        console.log('Erro ao enviar e-mail:', error);
+        alert("Erro ao enviar e-mail");
+    });
+
+}
+function limpar(){
+    document.getElementById("nome").value="";
+    document.getElementById("mail").value="";
+    document.getElementById("sugestao").value="";
+}
